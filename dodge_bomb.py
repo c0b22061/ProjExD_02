393c3e75ea5db6c473ffcbff57f4cd7774289090
taku <c0b22061a5@edu.teu.ac.jp>
2023-10-03 14:25:16 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -19,6 +19,7 @@ def main():
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
     x,y=random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    vx,vy=+5,+5
     bd_rct.center = (x,y)
     tmr = 0
     while True:
@@ -28,10 +29,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
